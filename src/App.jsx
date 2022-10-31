3a50dcd44438aceb3bc1ef7e47d68901738003e0
Francisco Cortez <franciscocortezdev@gmail.com>
2022-10-31 17:20:39 -0600

    Material UI package added

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,17 @@
 import './App.css'
-import { Formik, Field } from 'formik';
-import Select from 'react-select'
+import Form from './Components/Form'
+
 
 function App() {
 
 
 
 
-  const options = [
-    { value: 1, label: 'Chocolate' },
-    { value: 2, label: 'Strawberry' },
-    { value: 3, label: 'Vanilla' }
-  ]
-
+ 
   return (
 
-
-    <div>
-     <h1>Anywhere in your app!</h1>
-     <Formik
-       initialValues={{ email: '', password: '', modulo: null }}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Requerido';
-         }
-         if (values.modulo === null) {
-          errors.modulo = 'Debe elegir un modulo';
-        }
-         return errors;
-       }}
-       onSubmit={(values) => {
-         console.log(values)
-
-       }}
-     >
-
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         setFieldValue,
-         setFieldTouched
-       }) => (
-         <form onSubmit={handleSubmit}>
-          <div>
-            
-           <input
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-             />
-           {errors.email && touched.email && errors.email}
-          </div>
-          <div>
-
-           <input
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-             />
-           {errors.password && touched.password && errors.password}
-          </div>
-          {values.modulo}
-
-          <Select 
-          name='modulo'
-          options={options} 
-          value={options.filter(opt=> opt.value==values.modulo)}
-          onChange={m=>{
-            setFieldValue('modulo', m.value)
-          }}
-          onBlur={()=>{
-            setFieldTouched('modulo');
-          }}
-          />
-          {errors.modulo && touched.modulo && errors.modulo}
-          
-           <button type="submit" >
-             Submit
-           </button>
-         </form>
-       )}
-     </Formik>
-   </div>
-  )
+    <Form/>
+      )
 }
 
 export default App
