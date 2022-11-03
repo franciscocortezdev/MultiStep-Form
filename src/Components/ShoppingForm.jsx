af69b4ca8f61e913f93636a8728246bfffc3525c
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-03 17:27:11 -0600

    Form DatosPersonales

--- src/Components/ShoppingForm.jsx
+++ src/Components/ShoppingForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Typography, Grid, TextField} from '@mui/material'
+import { Grid, TextField} from '@mui/material'
 
 
 
@@ -12,104 +12,42 @@ export default function ShoppingForm({values, handleChange, handleSubmit}) {
     <>
 
 
-    <Typography variant="h6" gutterBottom>
-        Shipping address
-      </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <TextField
-            
-            name="firstName"
-            label="First name"
+            name="Nombre"
+            label="Nombre"
             fullWidth
-            autoComplete="given-name"
             variant="standard"
-            value={values.firstName}
+            value={values.Nombre}
             onChange={handleChange}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
             
-            name="lastName"
-            label="Last name"
+            name="Apellido"
+            label="Apellido"
             fullWidth
-            autoComplete="family-name"
             variant="standard"
-            value={values.lastName}
+            value={values.Apellido}
             onChange={handleChange}
           />
         </Grid>
         <Grid item xs={12}>
           <TextField
             
-            name="address1"
-            label="Address line 1"
+            name="Direccion"
+            label="Direccion"
             fullWidth
             autoComplete="shipping address-line1"
             variant="standard"
-            value={values.address1}
+            value={values.Direccion}
             onChange={handleChange}
           />
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="address2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="shipping address-line2"
-            variant="standard"
-            value={values.address2}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-            value={values.city}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-            value={values.state}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-            value={values.zip}
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-            value={values.country}
-            onChange={handleChange}
-          />
-        </Grid>
-        
+       
+                
       </Grid>
     </>
 
