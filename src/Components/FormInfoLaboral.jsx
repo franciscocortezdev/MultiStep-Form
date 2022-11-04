38e71816031e65161842846dd0d227e003bd28bd
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-04 11:02:47 -0600

    Formulario informacion laboral validado

--- src/Components/FormInfoLaboral.jsx
+++ src/Components/FormInfoLaboral.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid, TextField} from '@mui/material';
 
 
-export default function FormInfoLaboral({values, handleChange, handleSubmit, errors, touched, handleBlur}) {
+export default function FormInfoLaboral({values, handleChange, errors, touched, handleBlur, setFieldValue,setFieldTouched}) {
 
 
 
