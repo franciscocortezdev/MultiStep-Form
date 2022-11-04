ba7bb71e0b74fcb349efb2a7c275ba11104a7441
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-04 11:02:34 -0600

    Formulario de datos personales validados

--- src/Components/FormDPersonales.jsx
+++ src/Components/FormDPersonales.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Grid, TextField, InputLabel} from '@mui/material'
+import { Grid, TextField, InputLabel, FormHelperText} from '@mui/material'
+import Select from 'react-select'
 
 
 
 
-export default function FormDPersonales({values, handleChange, handleSubmit, errors, touched, handleBlur}) {
+export default function FormDPersonales({values, handleChange, errors, touched, handleBlur, setFieldValue,setFieldTouched}) {
 
-  console.log('touched.Nombre', (touched.Nombre && Boolean(errors.Nombre)))
-
+  const estadosCiviles = [
+    {
+      id: 1,
+      label: 'Soltero'
+    },
+    {
+      id: 2,
+      label: 'Casado'
+    },
+    {
+      id: 3,
+      label: 'Divorciado'
+    }
+  ]
   return (
     <>
     
@@ -67,8 +80,31 @@ export default function FormDPersonales({values, handleChange, handleSubmit, err
             onBlur={handleBlur}
           />
         </Grid>
+        <Grid item xs={12}>
+          <InputLabel >Estado civil</InputLabel>
+          <Select
+          placeholder={'Seleccione su estado civil'}
+          options={estadosCiviles}
+          getOptionValue={option => option.id}
+          getOptionLabel={option => option.label}
+          value = {
+            estadosCiviles.filter(opt => opt.id === values.estadoCivil)
+          }
+          onChange={m=>{
+            setFieldValue('estadoCivil', m.id)
+          }}
+          onBlur={()=>{
+            setFieldTouched('estadoCivil');
+          }}
+          styles={{
+            control: (provided, state) => ( errors.estadoCivil && touched.estadoCivil ? { ...provided, borderColor: 'red'}
+            : provided )
+          }}
+        />
+        {errors.estadoCivil && touched.estadoCivil && <FormHelperText error={true}>{errors.estadoCivil}</FormHelperText>}
+        </Grid>
        
-                
+        
       </Grid>
 
     </>
