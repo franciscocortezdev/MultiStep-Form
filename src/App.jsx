9a91726e182b64c0a9c1e61ef071fd4e1d6668e0
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-30 10:53:08 -0600

    Form formik and RHF

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 import Form from "./Components/formikForm/Form";
 import FormRHF from "./Components/reactHookForm/FormRHF";
+import { Tabs, Tab, Typography, Box } from "@mui/material/";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
-      {/* <Form /> */}
-
-      <FormRHF />
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Formik" />
+            <Tab label="React Hook Form" />
+          </Tabs>
+        </Box>
+        {value === 0 ? <Form /> : <FormRHF />}
+      </Box>
     </>
   );
 }
